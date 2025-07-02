4f3ded916e56706075f9994f0aedbb07b13ad46e
ChhairongCoding <chhinchhairong18@gmail.com>
2025-07-02 22:39:28 +0700

    Done home ui

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,5 +1,5 @@
 class AppRoutes {
-  static String gettingStartRoute = '/';
+  static String gettingStartRoute = '/getting';
   static String loginRoute = '/login';
   static String signUpRoute = '/signup';
   static String otpRoute = '/otp';
--- lib/views/main_views/main_controller.dart
+++ lib/views/main_views/main_controller.dart
@@ -1,12 +1,21 @@
+import 'package:e_commerce_app/views/cart_screen.dart';
 import 'package:e_commerce_app/views/favorite_screen.dart';
 import 'package:e_commerce_app/views/home_screen.dart';
+import 'package:e_commerce_app/views/profile_screen.dart';
+import 'package:e_commerce_app/views/shop_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class MainController extends GetxController {
   RxInt currentIndex = 0.obs;
 
-  final List<Widget> listScreens = [HomeScreen(), FavoriteScreen()];
+  final List<Widget> listScreens = [
+    HomeScreen(),
+    ShopScreen(),
+    FavoriteScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
 
   void toggleSwitch(int value) {
     currentIndex.value = value;
