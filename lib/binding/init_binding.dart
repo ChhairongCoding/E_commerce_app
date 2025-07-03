83dbcedc798af5e2319b87b621fcd4f42c909959
ChhairongCoding <chhinchhairong18@gmail.com>
2025-07-03 22:51:29 +0700

    Done with ui in search screen

--- lib/binding/init_binding.dart
+++ lib/binding/init_binding.dart
@@ -1,10 +1,10 @@
 import 'package:e_commerce_app/controllers/home_controller.dart';
 import 'package:e_commerce_app/controllers/login_controller.dart';
 import 'package:e_commerce_app/controllers/otp_controller.dart';
+import 'package:e_commerce_app/controllers/shop_controller.dart';
 import 'package:e_commerce_app/controllers/signup_controller.dart';
 import 'package:e_commerce_app/views/main_views/main_controller.dart';
 import 'package:e_commerce_app/services/auth_service.dart';
-import 'package:e_commerce_app/views/new_arrival_view/main_new_arrival_controller.dart';
 import 'package:get/get.dart';
 
 class InitBinding extends Bindings {
@@ -12,12 +12,11 @@ class InitBinding extends Bindings {
   void dependencies() {
     //controller
     Get.put(MainController());
-    Get.put(MainNewArrivalController());
     Get.put(LoginController());
     Get.put(SignupController());
     Get.put(OtpController());
     Get.put(HomeController());
-
+    Get.put(ShopController());
     //service
     Get.put(AuthService());
 
---
+++ lib/controllers/shop_controller.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+class ShopController extends GetxController {
+  List<Map<String, dynamic>> listCate = [
+    {
+      'name': 'Shoes',
+      'imageUrl':
+          'https://www.rebelsport.com.au/on/demandware.static/-/Sites-srg-internal-master-catalog/default/dwf19506fd/hof/250430-HPLP-Football-3D-Nike-SuperflyMbappe.png',
+    },
+    {
+      'name': 'Collection',
+      'imageUrl':
+          'https://www.indianultras.com/cdn/shop/files/CelticAmazon.png?v=1739370085&width=550',
+    },
+    {
+      'name':'Accessories',
+      'imageUrl':'https://static.vecteezy.com/system/resources/thumbnails/023/477/419/small_2x/ai-generative-collection-of-sports-equipment-commonly-sold-at-a-supermarket-circle-label-for-a-sports-goods-free-png.png',
+    }
+  ];
+
+  List<Map<String, List<String>>> listCateItem = [
+    {
+      'Shoes': ['Running', 'Soccer', 'Sneaker', 'Basketball'],
+      'Collection': ['Copa', 'F50', 'Superstar', 'Sportswear'],
+    },
+  ];
+}
